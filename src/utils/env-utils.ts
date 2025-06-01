6bb4a39327919dc4ce2f7d7f27a8bfe1297d4f19
conglinyizhi <cslbyivi@outlook.com>
2025-06-01 23:35:57 +0800

    fix(utils/env-utils): 修复国际化环境变量 Locale 的默认值设置为 'en'

--- src/utils/env-utils.ts
+++ src/utils/env-utils.ts
@@ -3,7 +3,7 @@ import envPaths from 'env-paths';
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
 /** 环境变量 LANG，用于国际化 */
-export const Locale = process.env.LANG;
+export const Locale = process.env.LANG || 'en';
 
 /** 环境变量，用于配置保存路径 */
 export const paths = envPaths('git-commit-for-llm');
